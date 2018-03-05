69ce9871be4756c669a0f26ec5560de158aa336c
Gautier Rouleau <gautier.rouleau@gmail.com>
2018-03-05 19:10:14 +0100

    [GRL]: added activities package + moved MainActivity

--- app/src/main/java/com/gmpvpc/android/activities/MainActivity.java
+++ app/src/main/java/com/gmpvpc/android/activities/MainActivity.java
@@ -1,7 +1,9 @@
-package com.gmpvpc.android;
+package com.gmpvpc.android.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.gmpvpc.android.R;
 
 public class MainActivity extends AppCompatActivity {
 
