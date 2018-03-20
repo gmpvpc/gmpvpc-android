6e3951a314ccd63dbce6cc52ca4262d3bd217d33
Gautier Rouleau <gautier.rouleau@gmail.com>
2018-03-20 20:52:49 +0100

    added icons and launchActivity function

--- app/src/main/java/com/gmpvpc/android/activities/MainActivity.java
+++ app/src/main/java/com/gmpvpc/android/activities/MainActivity.java
@@ -1,7 +1,9 @@
 package com.gmpvpc.android.activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.gmpvpc.android.R;
 
@@ -11,5 +13,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void startStaticsActivity() {
+        launchActivity("SeriesHistory");
+    }
+
+    public void launchActivity(String className) {
+        try {
+            Intent i = new Intent(this,
+                    Class.forName("com.gmpvpc.android.activities." + className));
+            startActivity(i);
+        } catch (ClassNotFoundException e) {
+            Toast.makeText(this, "Activity not found", Toast.LENGTH_SHORT).show();
+            e.printStackTrace();
+        }
     }
 }
