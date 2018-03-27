0492d1e677f26d59244c0400ec12a18193db6e17
Gautier Rouleau <gautier.rouleau@gmail.com>
2018-03-27 21:47:28 +0200

    #124 added useless async task to fake API request

--- app/src/main/java/com/gmpvpc/android/activities/SeriesStatistics.java
+++ app/src/main/java/com/gmpvpc/android/activities/SeriesStatistics.java
@@ -1,19 +1,120 @@
 package com.gmpvpc.android.activities;
 
+import android.app.ProgressDialog;
+import android.graphics.Color;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.RadioGroup;
 
 import com.gmpvpc.android.R;
 import com.gmpvpc.android.fragments.GraphFragment;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+
+import static java.lang.Thread.sleep;
 
 public class SeriesStatistics extends AppCompatActivity {
+    private static final int RADIO_ACCURACY = R.id.accuracy_radio_btn;
+    private static final int RADIO_SPEED = R.id.speed_radio_btn;
+    private static final int LINE_COLOR_BROWN = Color.parseColor("#FFE54000");
+
+    private RadioGroup dataType;
+    private GraphFragment graphFragment;
+    private ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_series_statistics);
 
-        GraphFragment graphFragment = (GraphFragment) getFragmentManager().findFragmentById(R.id.graph_fragment);
+        this.progressDialog = new ProgressDialog(SeriesStatistics.this);
+        this.progressDialog.setMessage("Loading...");
+        this.progressDialog.setCancelable(false);
 
+        this.dataType = findViewById(R.id.radio_group);
+
+        this.graphFragment = (GraphFragment) getFragmentManager().findFragmentById(R.id.graph_fragment);
+
+        this.dataType.setOnCheckedChangeListener(this::changeDataType);
+
+        this.dataType.check(RADIO_SPEED);
+    }
+
+    public void changeDataType(RadioGroup radioGroup, int radioButtonId) {
+        this.progressDialog.show();
+        switch (radioButtonId) {
+            case RADIO_ACCURACY:
+                this.loadAccuracy();
+                break;
+            case RADIO_SPEED:
+                this.loadSpeed();
+                break;
+        }
+    }
+
+    public void loadSpeed() {
+        LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[] {
+                new DataPoint(0, 1),
+                new DataPoint(1, 5),
+                new DataPoint(2, 3),
+                new DataPoint(3, 2),
+                new DataPoint(4, 6)
+        });
+
+        series.setColor(LINE_COLOR_BROWN);
+
+
+        new UselessAsyncTask(() -> {
+            this.graphFragment.getGraphView().removeAllSeries();
+            this.graphFragment.addSeries(series);
+            this.progressDialog.dismiss();
+        }).execute();
+    }
+
+    public void loadAccuracy() {
+        LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[] {
+                new DataPoint(0, 8),
+                new DataPoint(1, 9),
+                new DataPoint(2, 7),
+                new DataPoint(3, 6),
+                new DataPoint(4, 2)
+        });
+
+        series.setColor(LINE_COLOR_BROWN);
+
+        new UselessAsyncTask(() -> {
+            this.graphFragment.getGraphView().removeAllSeries();
+            this.graphFragment.addSeries(series);
+            this.progressDialog.dismiss();
+        }).execute();
+    }
+
+    private class UselessAsyncTask extends AsyncTask<Void, Void, Void> {
+        private IUseless useless;
+
+        private UselessAsyncTask(IUseless useless) {
+            this.useless = useless;
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            try {
+                sleep(3000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void voids) {
+            useless.doThat();
+        }
+    }
+
+    private interface IUseless {
+        void doThat();
     }
 }
--- app/src/main/java/com/gmpvpc/android/fragments/GraphFragment.java
+++ app/src/main/java/com/gmpvpc/android/fragments/GraphFragment.java
@@ -11,6 +11,7 @@ import android.view.ViewGroup;
 import com.gmpvpc.android.R;
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.series.BarGraphSeries;
+import com.jjoe64.graphview.series.BaseSeries;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
 
@@ -33,29 +34,16 @@ public class GraphFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_graph, container, false);
 
-        GraphView graph = view.findViewById(R.id.graph_fragment);
-        LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[] {
-                new DataPoint(0, 1),
-                new DataPoint(1, 5),
-                new DataPoint(2, 3),
-                new DataPoint(3, 2),
-                new DataPoint(4, 6)
-        });
-
-        BarGraphSeries<DataPoint> series2 = new BarGraphSeries<>(new DataPoint[] {
-                new DataPoint(0, 5),
-                new DataPoint(1, 3),
-                new DataPoint(2, -3),
-                new DataPoint(3, 2),
-                new DataPoint(4, 6),
-                new DataPoint(4, -1)
-        });
-        series2.setColor(Color.GREEN);
-
-        graph.addSeries(series2);
-        graph.addSeries(series);
+        this.graphView = view.findViewById(R.id.graph_fragment);
 
         return view;
     }
 
+    public void addSeries(BaseSeries<DataPoint> series) {
+        this.graphView.addSeries(series);
+    }
+
+    public GraphView getGraphView() {
+        return graphView;
+    }
 }
