96727172d68ce0f4e4f30100dbad87dc01a75d14
Gautier Rouleau <gautier.rouleau@gmail.com>
2018-03-28 19:05:14 +0200

    #127 added Global Statistics Activity

---
+++ app/src/main/java/com/gmpvpc/android/activities/GlobalStatisticsActivity.java
@@ -0,0 +1,15 @@
+package com.gmpvpc.android.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.gmpvpc.android.R;
+
+public class GlobalStatisticsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_global_statistics);
+    }
+}
